246179c0b903daaddacd48182bfbd8d4783167c5
edilbertocantuaria <edilbertounbfga@gmail.com>
2023-04-26 20:08:53 -0300

    Testing post function

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,6 +10,28 @@ app.use(express.json());
 app.use(cors())
 app.use(router);
 
+app.post("/participants", async (req, res) => {
+
+    const { name, email, password, passwordConfirm } = req.body;
+    const user = { name, email, password, passwordConfirm };
+
+    const userAlreadyRegistered = await db.collection('users').findOne({ email: email });
+    if (userAlreadyRegistered) return res.sendStatus(409);
+
+    const validation = singupUserSchema.validate(user);
+    if (validation.error) return res.sendStatus(422);
+
+    const passwordHash = bcrypt.hashSync(user.password, 10);
+
+    await db.collection("users").insertOne({
+        name: name.trim(),
+        email: email.trim(),
+        password: passwordHash.trim()
+    });
+
+    res.status(201).send("New user created successfully");
+
+})
 
 const PORT = 5000;
 app.listen(PORT, () => {
