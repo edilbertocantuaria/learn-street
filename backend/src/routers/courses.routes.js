549b4cec8a9832ab616cee975b13675a82adac05
LorenzoBaumgratz <lorenzobaumgratz@yahoo.com.br>
2023-04-27 18:26:19 -0300

    feat: getCourses complete

--- backend/src/routers/courses.routes.js
+++ backend/src/routers/courses.routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { validateSchema } from "../middlewares/validateSchema.js"
 import { courseSchema } from '../schemas/auth.schemas.js';
-import { createCourse } from '../controllers/courses.controllers.js';
+import { createCourse, getCursos } from '../controllers/courses.controllers.js';
 
 const courseRouter = Router();
 
 courseRouter.post("/courses", validateSchema(courseSchema), createCourse);
+courseRouter.get("/courses", getCursos);
 
 export default courseRouter;
