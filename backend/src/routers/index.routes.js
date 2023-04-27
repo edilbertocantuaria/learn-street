e9d44f9a6cbc160f37003450cba66dfdf3f0fb91
TiagoNovaes <tiagousnovaes@gmail.com>
2023-04-27 17:41:04 -0300

    feat: add course router

--- backend/src/routers/index.routes.js
+++ backend/src/routers/index.routes.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import authRouter from './auth.routes.js'
+import courseRouter from "./courses.routes.js";
 
 const router = Router();
 router.use(authRouter);
+router.use(courseRouter);
 
 export default router;
