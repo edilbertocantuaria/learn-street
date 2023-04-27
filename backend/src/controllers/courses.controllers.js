afff9af79f6cffba80e5e4307366d92c6f41e5d7
LorenzoBaumgratz <lorenzobaumgratz@yahoo.com.br>
2023-04-27 18:22:53 -0300

    feat: getCursos

--- backend/src/controllers/courses.controllers.js
+++ backend/src/controllers/courses.controllers.js
@@ -1,3 +1,5 @@
+import db from "../database/db";
+
 export async function createCourse(req, res) {
     const { title, price, description } = req.body;
 
@@ -16,6 +18,16 @@ export async function createCourse(req, res) {
         
         await db.collection("courses").insertOne(newCourse);
         res.sendStatus(201);
+
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+}
+
+export async function getCursos(req,res){
+    try { 
+        const cursos=await db.collection("courses").find().toArray()
+        res.status(201).send(cursos);
 
     } catch (err) {
         res.status(500).send(err.message)
