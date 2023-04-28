91639929c47444f6a47e96b0c97294c7d8944917
edilbertocantuaria <edilbertounbfga@gmail.com>
2023-04-28 17:52:55 -0300

    Requirement implemented: logout and getCart

--- backend/src/routers/purchase.routes.js
+++ backend/src/routers/purchase.routes.js
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import { validateSchema } from "../middlewares/validateSchema.js"
 import { checkoutSchema } from '../schemas/purchase.schemas.js';
-import { addCart, checkout, removeCart } from '../controllers/purchase.controllers.js';
+import { addCart, checkout, removeCart, getCart } from '../controllers/purchase.controllers.js';
 import { authValidation } from '../middlewares/authSchema.middleware.js';
 
 const purchaseRouter = Router();
 
 purchaseRouter.use(authValidation)
 purchaseRouter.post("/checkout", validateSchema(checkoutSchema), checkout);
+purchaseRouter.get("/cart",getCart);
 purchaseRouter.post("/cart",addCart);
 purchaseRouter.delete("/cart",removeCart);
 
