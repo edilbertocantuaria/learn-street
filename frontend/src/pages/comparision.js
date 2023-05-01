c45224dbb8434d5cf82904f9bf245fc44a435ebe
edilbertocantuaria <edilbertounbfga@gmail.com>
2023-05-01 11:55:05 -0300

    Updating front and backend code

--- frontend/src/pages/comparision.js
+++ frontend/src/pages/comparision.js
@@ -33,7 +33,7 @@ export default function Home() {
             setCourses(coursesAddedAtCart);
             setLoading(false);
         });
-        console.log(courses)
+
     }, []);
 
     useEffect(() => {
@@ -41,12 +41,7 @@ export default function Home() {
         console.log(courses.length);
         let finalValue = 0;
         courses.forEach((course) => {
-            if (typeof course.course_cost === "number") {
-                finalValue += course.course_cost;
-            } else {
-               finalValue += Number(course.course_cost);
-            }
-            console.log(typeof (finalValue))
+            finalValue += Number(course.course_cost)
         });
         setTotalValue(finalValue);
     }, [courses]);
@@ -72,8 +67,7 @@ export default function Home() {
                                             <div className="coursesInfo">
                                                 <div className="courseName">{course.course_name}
                                                 </div>
-                                                <div className="coursePrice">{
-                                                Number((course.course_cost)).toFixed(2).replace(".", ",")}</div>
+                                                <div className="coursePrice">{course.course_cost}</div>
                                             </div>
                                             <div className="divisionLine">
                                                 <div className="line"></div>
@@ -89,7 +83,7 @@ export default function Home() {
 
                     <div className="totalBuying">
                         <div className="total">Total</div>
-                        <div className="price">R$ {(totalValue).toFixed(2).replace(".", ",")} </div>
+                        <div className="price">R$ {totalValue}</div>
                     </div>
                 </div>
             </Main>
